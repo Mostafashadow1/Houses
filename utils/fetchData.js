abe8944d8b00c14e55b4e0449486675b78374b32
Msotafa Mohamed <60571462+Mostafashadow1@users.noreply.github.com>
2022-08-19 12:51:15 +0200

    Update fetchData.js

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -4,7 +4,7 @@ export const getData = async (url) => {
   const res = await axios.get(url, {
     headers: {
       "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
-      "X-RapidAPI-Host": process.env.RAPIDAPI_HOST,
+      "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
   return res.data;
